dccf87e797c12ff3d19054508589ace4e6b31e78
punyashah <65733108+punyashah@users.noreply.github.com>
2020-12-19 10:49:46 -0500

    Create WaitlistEntry.java
    Class for creating Waitlist objects

---
+++ WaitlistEntry.java
@@ -0,0 +1,43 @@
+import java.sql.Date;
+import java.sql.Timestamp;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Punya
+ */
+public class WaitlistEntry {
+    
+    private final String faculty;
+    private final Date date;
+    private final int seats;
+    private final Timestamp timestamp;
+    
+    public WaitlistEntry(String faculty, Date date, int seats, Timestamp timestamp){ //Constructor to set the values of the variables above
+        this.faculty = faculty;
+        this.date = date;
+        this.seats = seats;
+        this.timestamp = timestamp;
+    }
+    
+    public String getFaculty(){
+        return faculty;
+    }
+    
+    public Date getDate(){
+        return date;
+    }
+    
+    public int getSeats(){
+        return seats;
+    }
+    
+    public Timestamp getTimestamp(){
+        return timestamp;
+    }
+    
+}
